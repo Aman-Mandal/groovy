fecab1b53e239740c7504f550f35f0ef9419ed10
Aman-Mandal <002amankumar@gmail.com>
2022-09-26 10:56:22 +0530

    basic file structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default MyApp;
