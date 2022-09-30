2361bb8a23b3eb1094023f42793beaadb35cfb47
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-30 19:36:30 +0530

    made explore page banner added background image to right section excluding sidebar trending page banner dynamic fetch and onclick change needs to be done

--- pages/Explore.jsx
+++ pages/Explore.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Trending from "../components/Trending/Trending";
 
 const Explore = () => {
   return (
-    <div>Explore</div>
-  )
-}
+    <div className="h-screen flex flex-col gap-8 mt-14 mx-8 ">
+      <h2 className="text-2xl font-normal text-gray-100">Trending New Hits</h2>
 
-export default Explore
+      <Trending />
+    </div>
+  );
+};
+
+export default Explore;
