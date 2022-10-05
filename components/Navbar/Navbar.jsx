000d80abeae65e10f187d756ca4a7e4fd6b1f626
Rohan Hazari <hazarirohan2002@gmail.com>
2022-10-05 23:42:09 +0530

    Update Navbar.jsx

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 
     handleResize();
 
-    return () => window.removeEventListscreenSizener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
