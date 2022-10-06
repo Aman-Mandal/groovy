236eabd32618d556d73f9eaf3033948b89eb3895
ImKunal13z <2002kunalmondal13@gmail.com>
2022-10-06 20:10:15 +0530

    feat(navbar): responsive

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -51,8 +51,8 @@ const Navbar = () => {
 
   return (
     <div className={`w-full flex items-center justify-between my-4 px-8`}>
-      <div className="flex items-center gap-32">
-        <div className="flex items-center gap-8">
+      <div className="flex items-center lg:gap-32 gap-5">
+        <div className="flex items-center lg:gap-8 gap-4">
           <IconButton
             type="button"
             className="relative swatch_text-primary rounded-full"
@@ -61,23 +61,23 @@ const Navbar = () => {
             <MenuIcon />
           </IconButton>
           <Link href="/">
-            <a className="uppercase text-lg font-light swatch_text-secondary">
+            <a className="uppercase lg:text-lg md:text-base text-xs font-light swatch_text-secondary">
               music
             </a>
           </Link>
           <Link href="/">
-            <a className="uppercase text-lg font-light swatch_text-primary">
+            <a className="uppercase lg:text-lg md:text-base text-xs font-light swatch_text-primary">
               podcast
             </a>
           </Link>
           <Link href="/">
-            <a className="uppercase text-lg font-light swatch_text-primary">
+            <a className="uppercase lg:text-lg md:text-base text-xs font-light swatch_text-primary">
               live
             </a>
           </Link>
         </div>
 
-        <div className="flex items-center p-3 gap-2 border-[1px] border-gray-500 rounded-md swatch_bg-brown">
+        <div className="sm:flex hidden items-center p-3 gap-2 border-[1px] border-gray-500 rounded-md swatch_bg-brown ">
           <SearchIcon className="text-gray-300" fontSize="small" />
           <input
             type="text"
@@ -96,13 +96,13 @@ const Navbar = () => {
         <NavButton icon={<SettingsIcon />} title={"Settings"} func={() => {}} />
 
         <Tooltip title="Profile">
-          <div className="flex items-center gap-2 swatch_bg-brown p-2 ml-2 px-3 rounded">
+          <div className="flex items-center gap-2 swatch_bg-brown md:p-2 ml-2 md:px-3 rounded">
             <Avatar
               src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
               variant="square"
               className="w-[30px] h-[30px] rounded"
             />
-            <span className="text-sm text-gray-300 capitalize">
+            <span className="text-sm text-gray-300 capitalize md:block hidden">
               Dave Cooper
             </span>
           </div>
