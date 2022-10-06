d4e17a531be781ad4a25817570e6058877c1ae7d
Manav Naharwal <manavnaharwalp@gmail.com>
2022-10-06 23:51:39 +0530

    Fix homepage scrolling bug

--- pages/Explore.jsx
+++ pages/Explore.jsx
@@ -1,16 +1,18 @@
-import { TopArtists, Genres, TopCharts, Player, Trending } from "../components";
-import { useStateContext } from "../contexts/ContextProvider";
+import { TopArtists, Genres, TopCharts, Player, Trending } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Explore = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
 
   return (
     <div
-      className={`flex flex-col gap-4 mt-8 px-8 ${activeMenu && "md:ml-72"}`}
+      className={`flex flex-col gap-4 mt-8 px-8 overflow-auto ${
+        activeMenu && 'md:ml-72'
+      }`}
     >
       <Trending />
 
-      <div className="grid grid-cols-6 gap-x-6 gap-4 mt-2">
+      <div className='grid grid-cols-6 gap-x-6 gap-4 mt-2'>
         <TopArtists />
         <Player />
         <Genres />
