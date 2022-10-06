859b0356e2476f40efeaacfb09e10a1f104d5b78
Manav Naharwal <manavnaharwalp@gmail.com>
2022-10-07 00:42:48 +0530

    Put songs on player from trending and top charts

--- components/TopCharts/TopCharts.jsx
+++ components/TopCharts/TopCharts.jsx
@@ -1,16 +1,27 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { charts } from "../../data/data";
-import AddBoxIcon from "@mui/icons-material/AddBox";
-
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { charts } from '../../data/data';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { useStateContext } from '../../contexts/ContextProvider';
 const TopCharts = () => {
+  const { setcurrentSong } = useStateContext();
+
+  const selectSongHandler = (song) => {
+    setcurrentSong((prev) => {
+      return {
+        ...prev,
+        ...song,
+      };
+    });
+  };
+
   return (
-    <div className="row-span-1 col-span-2 swatch_bg-brown p-4 rounded-md flex flex-col gap-4 overflow-y-scroll">
-      <div className="flex items-center justify-between">
-        <p className="text-lg text-gray-100 font-medium">Top Charts</p>
-        <Link href="/Albums">
-          <a className="text-sm swatch_text-primary">See all</a>
+    <div className='row-span-1 col-span-2 swatch_bg-brown p-4 rounded-md flex flex-col gap-4 overflow-y-scroll'>
+      <div className='flex items-center justify-between'>
+        <p className='text-lg text-gray-100 font-medium'>Top Charts</p>
+        <Link href='/Albums'>
+          <a className='text-sm swatch_text-primary'>See all</a>
         </Link>
       </div>
       <div>
@@ -18,30 +29,39 @@ const TopCharts = () => {
           (chart, index) =>
             index < 3 && (
               <div
-                className="gap-2 flex items-center justify-between mb-4"
+                className='gap-2 flex items-center justify-between mb-4'
                 key={chart.songName}
+                onClick={() =>
+                  selectSongHandler({
+                    image: chart.img,
+                    title: chart.songName,
+                    artist: chart.artistName,
+                  })
+                }
               >
-                <div className="flex items-center gap-3">
-                  <div className="rounded-md w-16 h-16 overflow-hidden">
+                <div className='flex items-center gap-3 cursor-pointer'>
+                  <div className='rounded-md w-16 h-16 overflow-hidden'>
                     <Image src={chart.img} alt={chart.songName} />
                   </div>
 
                   <div className='flex flex-col'>
-                    <p className="text-gray-100 text-lg font-medium">{chart.songName}</p>
-                    <small className="text-gray-400 text-xs">
+                    <p className='text-gray-100 text-lg font-medium'>
+                      {chart.songName}
+                    </p>
+                    <small className='text-gray-400 text-xs'>
                       {chart.artistName}
                     </small>
                   </div>
                 </div>
                 <div className='flex'>
-                  <div className="flex items-center gap-3">
-                    <small className="swatch_text-primary text-xs mr-2">
+                  <div className='flex items-center gap-3'>
+                    <small className='swatch_text-primary text-xs mr-2'>
                       {chart.time}
                     </small>
-                    <p className="swatch_text-primary w-6 h-6 flex items-center justify-center text-xl mr-2 border-[1px] border-[#192CFD]">
+                    <p className='swatch_text-primary w-6 h-6 flex items-center justify-center text-xl mr-2 border-[1px] border-[#192CFD]'>
                       +
                     </p>
-                    <AddBoxIcon className="mr-2 text-gray-400" />
+                    <AddBoxIcon className='mr-2 text-gray-400' />
                   </div>
                 </div>
               </div>
--- components/Trending/Trending.jsx
+++ components/Trending/Trending.jsx
@@ -1,9 +1,11 @@
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { IconButton } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { songs } from "../../data/trendingSongsData";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { IconButton } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { songs } from '../../data/trendingSongsData';
+import { useStateContext } from '../../contexts/ContextProvider.js';
+import SampleImage from '../../public/default_player_image.webp';
 
 const Trending = () => {
   const [index, setIndex] = useState(0);
@@ -11,69 +13,95 @@ const Trending = () => {
   const [artist, setArtist] = useState(songs[0].artist);
   const [noOfPlays, setNoOfPlays] = useState(songs[0].noOfPlays);
   const noOfSongs = songs.length;
+  const { setcurrentSong } = useStateContext();
+
   useEffect(() => {
-    const dots = document.getElementsByClassName("trend-circle");
+    const dots = document.getElementsByClassName('trend-circle');
     for (let x = 0; x < dots.length; x++) {
-      dots[x].classList.remove("bg-gray-600");
-      dots[x].classList.add("bg-gray-100");
+      dots[x].classList.remove('bg-gray-600');
+      dots[x].classList.add('bg-gray-100');
     }
-    dots[index].classList.remove("bg-gray-100");
-    dots[index].classList.add("bg-gray-600");
+    dots[index].classList.remove('bg-gray-100');
+    dots[index].classList.add('bg-gray-600');
     setSongName(songs[index].title);
     setArtist(songs[index].artist);
     setNoOfPlays(songs[index].noOfPlays);
   }, [index]);
 
+  const selectSongHandler = (song) => {
+    setcurrentSong((prev) => {
+      return {
+        ...prev,
+        ...song,
+        image: song.image ? song.image : SampleImage,
+      };
+    });
+  };
+
   return (
-    <div className="flex items-center justify-between pl-6">
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-7xl font-extrabold text-gray-100">{songName}</h1>
-          <div className="flex gap-4 items-center">
-            <p className="text-3xl text-gray-100">{artist}</p>
-            <span className="text-gray-400 text-xl tracking-tighter">
+    <div className='flex items-center justify-between pl-6'>
+      <div className='flex flex-col gap-6'>
+        <div className='flex flex-col gap-2'>
+          <h1 className='text-7xl font-extrabold text-gray-100'>{songName}</h1>
+          <div className='flex gap-4 items-center'>
+            <p className='text-3xl text-gray-100'>{artist}</p>
+            <span className='text-gray-400 text-xl tracking-tighter'>
               {noOfPlays} Plays
             </span>
           </div>
         </div>
 
-        <div className="flex gap-5 items-center">
-          <button className="swatch_bg-secondaryLight text-gray-100 text-md p-4 px-6 rounded-full font-bold hover:bg-[#234DA2]/95">
+        <div className='flex gap-5 items-center'>
+          <button
+            className='swatch_bg-secondaryLight text-gray-100 text-md p-4 px-6 rounded-full font-bold hover:bg-[#234DA2]/95'
+            onClick={() =>
+              selectSongHandler({
+                title: songName,
+                artist: artist,
+              })
+            }
+          >
             Listen Now
           </button>
-          <button className="rounded-full p-2 border-2 border-gray-300">
-            <FavoriteIcon className="swatch_text-secondary" />
+          <button className='rounded-full p-2 border-2 border-gray-300'>
+            <FavoriteIcon className='swatch_text-secondary' />
           </button>
         </div>
       </div>
 
-      <div className="flex flex-col gap-12 items-center">
-        <div className="flex flex-col items-center gap-4">
+      <div className='flex flex-col gap-12 items-center'>
+        <div className='flex flex-col items-center gap-4'>
           {songs.map((item, ind) => {
             if (ind == 0) {
               return (
-                <span className="w-1 h-1 bg-gray-600 cursor-pointer rounded-full trend-circle"></span>
+                <span
+                  key={ind}
+                  className='w-1 h-1 bg-gray-600 cursor-pointer rounded-full trend-circle'
+                ></span>
               );
             }
             return (
-              <span className="w-1 h-1 bg-gray-100 cursor-pointer rounded-full trend-circle"></span>
+              <span
+                key={ind}
+                className='w-1 h-1 bg-gray-100 cursor-pointer rounded-full trend-circle'
+              ></span>
             );
           })}
         </div>
 
-        <div className="flex flex-col items-center gap-4">
+        <div className='flex flex-col items-center gap-4'>
           <IconButton
-            className="bg-[#080808] text-gray-100"
+            className='bg-[#080808] text-gray-100'
             onClick={() => {
-              index == 0 ? "" : setIndex(index - 1);
+              index == 0 ? '' : setIndex(index - 1);
             }}
           >
             <KeyboardArrowUpIcon />
           </IconButton>
           <IconButton
-            className="bg-[#080808] text-gray-100"
+            className='bg-[#080808] text-gray-100'
             onClick={() => {
-              index == noOfSongs - 1 ? "" : setIndex(index + 1);
+              index == noOfSongs - 1 ? '' : setIndex(index + 1);
             }}
           >
             <KeyboardArrowDownIcon />
