2e974913bb03576eb8b560632c0a0739900c7f49
unknown <jagadyudhaawali@gmail.com>
2022-10-06 21:24:59 +0700

    fix: search bar disturbs other navbar menu

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -15,12 +15,12 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import { useStateContext } from "../../contexts/ContextProvider";
 import MenuIcon from "@mui/icons-material/Menu";
 import NotificationModal from "../NotificationModal/NotificationModal";
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
-import { Fragment } from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { Fragment } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import { Image } from "next/image";
-import classNames from 'classnames';
+import classNames from "classnames";
 import SearchBar from "./SearchBar";
 import { songs } from "../../data/trendingSongsData";
 const NavButton = ({ title, icon, func }) => {
@@ -72,22 +72,17 @@ const Navbar = () => {
   };
   const { data: session, status } = useSession();
 
-
   if (status === "authenticated") {
-
-    SessionObj.name = ` ${session.user.name}`
-    SessionObj.email = `${session.user.email}`
-    SessionObj.image = `${session.user.image}`
+    SessionObj.name = ` ${session.user.name}`;
+    SessionObj.email = `${session.user.email}`;
+    SessionObj.image = `${session.user.image}`;
   }
-
-
-
-
 
   return (
     <div
-      className={`flex items-center justify-between my-4 px-8 ${activeMenu && "md:ml-72"
-        }`}
+      className={`flex relative items-center justify-between my-4 px-8 ${
+        activeMenu && "md:ml-72"
+      }`}
     >
       <div className="flex items-center gap-32">
         <div className="flex items-center gap-8">
@@ -109,16 +104,16 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div className="hidden lg:flex items-center p-3 gap-2 border-[1px] border-gray-500 rounded-md swatch_bg-brown">
-          {/* <SearchIcon className="text-gray-300" fontSize="small" />
+        <div className="absolute top-0 left-0 right-0 mx-auto w-fit">
+          <div className="hidden lg:block top-0 rounded-lg p-3 gap-2 border-[1px] border-gray-500 swatch_bg-brown">
+            {/* <SearchIcon className="text-gray-300" fontSize="small" />
           <input
             type="text"
             placeholder="Type here to search"
             className="outline-none border-none text-gray-300 placeholder-gray-300 bg-transparent"
           /> */}
-          <SearchBar
-            placeholder="Search the songs here"
-            data={songs} />
+            <SearchBar placeholder="Search the songs here" data={songs} />
+          </div>
         </div>
       </div>
 
@@ -128,19 +123,21 @@ const Navbar = () => {
           title={"Notifications"}
           func={() => handleClick("notification")}
         />
-        <NavButton icon={<SettingsIcon />} title={"Settings"} func={() => { }} />
-        <div className="flex items-center space-x-4 
-        justify-end text-gray-500">
+        <NavButton icon={<SettingsIcon />} title={"Settings"} func={() => {}} />
+        <div
+          className="flex items-center space-x-4 
+        justify-end text-gray-500"
+        >
           {/* borderRadius :'10px' */}
 
-          < img src={SessionObj.image} alt="none" style={{ width: 40, height: 40, borderRadius: '50px' }} />
-          <p className="hidden md:inline cursor-pointer text-white" >
-
-
+          <img
+            src={SessionObj.image}
+            alt="none"
+            style={{ width: 40, height: 40, borderRadius: "50px" }}
+          />
+          <p className="hidden md:inline cursor-pointer text-white">
             {SessionObj.name}
           </p>
-
-
 
           <Tooltip title="Profile">
             <Menu as="div" className="relative inline-block text-left">
@@ -148,7 +145,10 @@ const Navbar = () => {
                 {/* Login singup */}
                 <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
                   Login/Signup
-                  <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                  <ChevronDownIcon
+                    className="-mr-1 ml-2 h-5 w-5"
+                    aria-hidden="true"
+                  />
                 </Menu.Button>
               </div>
 
@@ -168,8 +168,10 @@ const Navbar = () => {
                         <a
                           href="#"
                           className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
                           )}
                         >
                           Login
@@ -182,22 +184,26 @@ const Navbar = () => {
                         <a
                           href="#"
                           className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
                           )}
                         >
                           License
                         </a>
                       )}
-                    </Menu.Item >
+                    </Menu.Item>
                     <form method="POST" action="#">
                       <Menu.Item onClick={signOut}>
                         {({ active }) => (
                           <button
                             type="submit"
                             className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block w-full text-left px-4 py-2 text-sm'
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block w-full text-left px-4 py-2 text-sm"
                             )}
                           >
                             Sign out
@@ -209,7 +215,6 @@ const Navbar = () => {
                 </Menu.Items>
               </Transition>
             </Menu>
-
           </Tooltip>
         </div>
         {isClicked.notification && <NotificationModal />}
