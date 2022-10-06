a51811dd13e096414ab166549f27018baf467fe7
nidhikumari <kumari9936nidhi@gmail.com>
2022-10-06 17:30:08 +0530

    SearchBar is working now

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -19,8 +19,10 @@ import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { Fragment } from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
-import { Image } from  "next/image";
+import { Image } from "next/image";
 import classNames from 'classnames';
+import SearchBar from "./SearchBar";
+import { songs } from "../../data/trendingSongsData";
 const NavButton = ({ title, icon, func }) => {
   return (
     <Tooltip title={title}>
@@ -63,20 +65,20 @@ const Navbar = () => {
     }
   }, [screenSize]);
 
-  const SessionObj={
-    name : "",
-    email :"",
+  const SessionObj = {
+    name: "",
+    email: "",
     image: "",
   };
-  const {  data :session, status } = useSession();
-  
-  
-    if (status === "authenticated") {
-     
-        SessionObj.name  = ` ${session.user.name}`
-        SessionObj.email =`${session.user.email}`
-        SessionObj.image =`${session.user.image}`
-      }
+  const { data: session, status } = useSession();
+
+
+  if (status === "authenticated") {
+
+    SessionObj.name = ` ${session.user.name}`
+    SessionObj.email = `${session.user.email}`
+    SessionObj.image = `${session.user.image}`
+  }
 
 
 
@@ -84,9 +86,8 @@ const Navbar = () => {
 
   return (
     <div
-      className={`flex items-center justify-between my-4 px-8 ${
-        activeMenu && "md:ml-72"
-      }`}
+      className={`flex items-center justify-between my-4 px-8 ${activeMenu && "md:ml-72"
+        }`}
     >
       <div className="flex items-center gap-32">
         <div className="flex items-center gap-8">
@@ -109,12 +110,15 @@ const Navbar = () => {
         </div>
 
         <div className="hidden lg:flex items-center p-3 gap-2 border-[1px] border-gray-500 rounded-md swatch_bg-brown">
-          <SearchIcon className="text-gray-300" fontSize="small" />
+          {/* <SearchIcon className="text-gray-300" fontSize="small" />
           <input
             type="text"
             placeholder="Type here to search"
             className="outline-none border-none text-gray-300 placeholder-gray-300 bg-transparent"
-          />
+          /> */}
+          <SearchBar
+            placeholder="Search the songs here"
+            data={songs} />
         </div>
       </div>
 
@@ -124,89 +128,89 @@ const Navbar = () => {
           title={"Notifications"}
           func={() => handleClick("notification")}
         />
-        <NavButton icon={<SettingsIcon />} title={"Settings"} func={() => {}} />
+        <NavButton icon={<SettingsIcon />} title={"Settings"} func={() => { }} />
         <div className="flex items-center space-x-4 
         justify-end text-gray-500">
-               {/* borderRadius :'10px' */}
-               
-               < img src={SessionObj.image} alt="none"  style={{width: 40, height: 40,borderRadius :'50px'}}/>
-               <p  className="hidden md:inline cursor-pointer text-white" >
-              
-                
-                {SessionObj.name}
-               </p>
-     
-   
-        
-        <Tooltip title="Profile">
-        <Menu as="div" className="relative inline-block text-left">
-      <div>
-        {/* Login singup */}
-        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-          Login/Signup
-          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-        </Menu.Button>
-      </div>
+          {/* borderRadius :'10px' */}
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            <Menu.Item onClick={signIn}>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Login
-                </a>
-              )}
-            </Menu.Item>
-            
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  License
-                </a>
-              )}
-            </Menu.Item >
-            <form method="POST" action="#">
-              <Menu.Item onClick={signOut}>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full text-left px-4 py-2 text-sm'
-                    )}
-                  >
-                    Sign out
-                  </button>
-                )}
-              </Menu.Item>
-            </form>
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+          < img src={SessionObj.image} alt="none" style={{ width: 40, height: 40, borderRadius: '50px' }} />
+          <p className="hidden md:inline cursor-pointer text-white" >
 
-        </Tooltip>
+
+            {SessionObj.name}
+          </p>
+
+
+
+          <Tooltip title="Profile">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                {/* Login singup */}
+                <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                  Login/Signup
+                  <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                </Menu.Button>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="py-1">
+                    <Menu.Item onClick={signIn}>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'block px-4 py-2 text-sm'
+                          )}
+                        >
+                          Login
+                        </a>
+                      )}
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'block px-4 py-2 text-sm'
+                          )}
+                        >
+                          License
+                        </a>
+                      )}
+                    </Menu.Item >
+                    <form method="POST" action="#">
+                      <Menu.Item onClick={signOut}>
+                        {({ active }) => (
+                          <button
+                            type="submit"
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block w-full text-left px-4 py-2 text-sm'
+                            )}
+                          >
+                            Sign out
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </form>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+
+          </Tooltip>
         </div>
         {isClicked.notification && <NotificationModal />}
       </div>
---
+++ components/Navbar/SearchBar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import SearchIcon from "@mui/icons-material/Search";
+
+function SearchBar({ placeholder, data }) {
+
+    const [filterSongs, setFilterSongs] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    function handleFilter(event) {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        const newFilter = data.filter((item) => {
+            return item.title.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if (searchWord === "") {
+            setFilterSongs([]);
+        } else {
+            setFilterSongs(newFilter)
+        }
+    }
+
+
+    return (
+        <div>
+            <form className="search">
+                <div>
+                    <input
+                        className="outline-none border-none text-gray-300 placeholder-gray-300 bg-transparent"
+                        type="text"
+                        placeholder={placeholder}
+                        onChange={handleFilter}
+                        value={wordEntered}
+                    />
+                    <SearchIcon className="text-gray-300" fontSize="small" />
+                </div>
+
+                {filterSongs?.length != 0 && (
+                    <div className="dataItems">
+                        {filterSongs.slice(0, 15).map((item, key) => {
+                            return (
+
+                                <a href={item.artist} className="text-gray-300">
+                                    <p>{item.title}</p>
+                                </a>
+
+                            )
+                        })}
+                    </div>
+                )}
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
