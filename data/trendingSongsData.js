30c98abf95a15b89168f7b1b153621d58ebe6ee0
Prajwal <prajwaldhule36@gmail.com>
2022-10-07 14:45:45 +0530

    added figma file for album page and fixed a typo in trendingSongsData.js

--- data/trendingSongsData.js
+++ data/trendingSongsData.js
@@ -1,27 +1,27 @@
 export const songs = [
   {
-    title: "In My Feelings",
-    artist: "Camila Caebllo",
-    noOfPlays: "63 Million",
+    title: 'In My Feelings',
+    artist: 'Camila Cabello',
+    noOfPlays: '63 Million',
   },
   {
-    title: "Crying for Rain",
-    artist: "Minami",
-    noOfPlays: "85 Million",
+    title: 'Crying for Rain',
+    artist: 'Minami',
+    noOfPlays: '85 Million',
   },
   {
-    title: "The Perfect Girl",
-    artist: "Mareux",
-    noOfPlays: "12 Million",
+    title: 'The Perfect Girl',
+    artist: 'Mareux',
+    noOfPlays: '12 Million',
   },
   {
-    title: "After Dark",
-    artist: "Mr Kitty",
-    noOfPlays: "42 Million",
+    title: 'After Dark',
+    artist: 'Mr Kitty',
+    noOfPlays: '42 Million',
   },
   {
-    title: "Rumbling",
-    artist: "SIM",
-    noOfPlays: "60 Million",
+    title: 'Rumbling',
+    artist: 'SIM',
+    noOfPlays: '60 Million',
   },
 ];
