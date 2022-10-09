8bc0f802ef25555fc2d22b7c1783092a74d8a04f
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-09 16:04:20 +0530

    made top charts scrollable

--- components/TopCharts/TopCharts.jsx
+++ components/TopCharts/TopCharts.jsx
@@ -45,14 +45,14 @@ const TopCharts = () => {
       </div>
     );
   return (
-    <div className='row-span-1 col-span-2 swatch_bg-brown p-4 rounded-md flex flex-col gap-4 overflow-y-scroll max-h-80'>
+    <div className='row-span-1 col-span-2 swatch_bg-brown p-4 rounded-md flex flex-col gap-4'>
       <div className='flex items-center justify-between'>
         <p className='text-lg text-gray-100 font-medium'>Top Charts</p>
         <Link href='/Albums'>
           <a className='text-sm swatch_text-primary'>See all</a>
         </Link>
       </div>
-      <div>
+      <div className='overflow-y-scroll max-h-80'>
         {topCharts.map((chart, index) => (
           <div
             className='gap-2 flex items-center justify-between mb-4'
