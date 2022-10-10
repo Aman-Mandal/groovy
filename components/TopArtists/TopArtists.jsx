10eb4648ec7e8757ab0eb71814b86b796dc1c3e8
Aadarsh Thakur <95094057+Aadarsh805@users.noreply.github.com>
2022-10-10 16:30:05 +0530

    Update TopArtists.jsx

--- components/TopArtists/TopArtists.jsx
+++ components/TopArtists/TopArtists.jsx
@@ -8,19 +8,19 @@ const TopArtists = () => {
   console.log(topTrending);
 
   return (
-    <div className='lg:col-span-4 col-span-full swatch_bg-brown pt-4 pr-b pl-4 rounded-md flex flex-col gap-4 w-full'>
+    <div className='lg:col-span-4 col-span-full swatch_bg-brown py-4 rounded-md flex flex-col gap-4 w-full'>
       {isFetching ? (
         <Loader />
       ) : (
         <>
-          <div className='flex items-center justify-between'>
+          <div className='flex items-center justify-between px-4'>
             <p className='text-lg text-gray-100 font-medium'>Top Artists</p>
             <Link href='/Artists'>
               <a className='text-sm swatch_text-primary'>See all</a>
             </Link>
           </div>
 
-          <div className='flex items-center justify-between overflow-auto py-2'>
+          <div className='flex items-center justify-between overflow-auto py-2 pl-4'>
             {topTrending.map(
               (artist, key) =>
                 key > 8 && (
