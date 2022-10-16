771ecc76c38125630d4942e4dbbbc05dd15de1ed
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-16 19:19:46 +0530

    linked genre-nav dynamically to genreCategory

---
+++ components/GenreNav/GenreNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+
+const genreCategory = [
+  'podcasts',
+  'charts',
+  'new-releases',
+  'discover',
+  'concerts',
+];
+const GenreNav = () => {
+  return (
+    <nav class='/Genres'>
+      <div class='container flex items-center justify-start p-6 mx-auto text-white capitalize'>
+        {genreCategory.map((category) => (
+          <Link href={`/Genres/${category}`}>
+            <a class='text-white border-b-2 border-blue-500 mx-1.5 sm:mx-6 uppercase'>
+              {category}
+            </a>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default GenreNav;
